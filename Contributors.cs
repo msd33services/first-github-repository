727df4807f2343b4a5b2ca9d2ead0402c3e3d236
MbuguaDennis <0001@mamal.nbi>
2025-08-13 10:09:09 +0300

    Add Dennis to contributors list

---
+++ Contributors.cs
@@ -0,0 +1,10 @@
+public static class Contributors {
+    public static void PrintContributors()
+    {
+        Console.WriteLine("Contributors:");
+        Console.WriteLine(" - ChatGPT");
+        Console.WriteLine(" - Mbugua Dennis");
+        // Add your name here
+
+    }
+}
